202d209a1bb3c8d02fb760b4bcf31e47d46eb9b1
DarkProrok <Seroshtanmakcs@gmail.com>
2023-06-30 03:05:30 +0300

    category_complete

--- Shop/Person/forms.py
+++ Shop/Person/forms.py
@@ -1,19 +1,12 @@
-from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+from Person.models import CustomUser
 
-# Create your forms here.
 
-class NewUserForm(UserCreationForm):
-	email = forms.EmailField(required=True)
 
+
+class CustomUserCreationForm(UserCreationForm):
 	class Meta:
-		model = User
-		fields = ("username", "email", "password1", "password2")
-
-	def save(self, commit=True):
-		user = super(NewUserForm, self).save(commit=False)
-		user.email = self.cleaned_data ['email']
-		if commit:
-			user.save()
+		model = CustomUser
+		fields = ('username', 'fio', 'gender', 'birth_date')
